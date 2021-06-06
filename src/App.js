8a4adf723e89cdee2d62e8c3fc99c758f98a6f64
Hyde Zhang <xueshi.zhang90@gmail.com>
2021-06-07 00:36:04 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Time to spin
+        </a>
+      </header>
+      <p>This is Hyde.</p>
+      <p>Nobody forces you to read on but obviously you are interested. Thanks. &#128275;</p>
+      <br />
+      <p>Among many other things, I always wanted to learn about web development after graduating with computer science degrees.</p>
+      <p>Yeah "how" indeed ... think about it. What you ended up doing everyday.</p>
+      <br />
+      <p>Also after graduation, I realised how important it is to document all the problems you've solved.</p>
+      <p>Yes what a surprise ... I wish life happened to me differently, or don't happen to me at all ...</p>
+      <br />
+      <p>Nevertheless, however slow the idea finally came: Why not "one stone" them by building a web site of your own.</p>
+      <br />
+      <p>Not a bad idea but it drowned in the <u>ignorance of "how to grow as an intelligent being"</u> - which is just a fancy way of saying:</p>
+      <p>Procrastination</p>
+      <p>Emotionally unstable</p>
+      <p>Magnet of distractions</p>
+      <p>Lack of the ability to control myself</p>
+      <p>being a thermometer not thermostat</p>
+      <br />
+      <p>Social dilemma ? Perhaps. The root cause is not that at all.</p>
+      <p>I have smart phone, laptop, desktop, office chair, standing desk, dual monitors, clarinet, electronic keyboard, yoga mat, Echo Plus, Echo dot, air purifier, humidifier ... a balcony with nice view ... but I don't have "Me"</p>
+      <p>My life has been burning for other people's agenda. Well, that's mostly because I don't have one for myself.</p>
+      <br />
+      <p>Time to stop this nonsense. This is my objectives, my design, my plan, my steps, my routine, my ritual. This is Me and only Me.</p>
+      <br />
+      <p>All these is actually for helping with the website design/content creation ... don't get too emotional &#128515;</p>
+    </div>
+  );
+}
+
+export default App;
